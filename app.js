cea95a72bc9ac28f5ab72d4005fc4e39fe33737e
AskarMadanov <69020975+AskarMadanov@users.noreply.github.com>
2021-11-30 18:21:09 -0600

    init

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 
-const exphbs = require('express-handlebars');
-const express = require('express')
-const app = express()
-var hbs = exphbs.create({
-    defaultLayout: 'main',
-});
-app.engine('handlebars', hbs.engine);
+/** Import dependencies */
+const {engine} = require('express-handlebars');
+const express = require('express');
+
+/** Init app */
+const app = express();
+
+/** Middlewares and Setups */
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-//app.js
 
+/** Routes */
 app.get('/', (req,res) => {
-    res.send('home', { msg: 'Handlebars are Cool!' })
+    res.render('home', { msg: 'Handlebars are Cool!' })
 })
 
+
+/** Listen */
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
